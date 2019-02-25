04301e7fabafef6488e6f57fd9d383d1890eeb01
Isarell <israeljuniorbbl123@gmail.com>
2019-02-25 08:37:20 -0300

    update

--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -11,7 +11,7 @@ import { MOVIELIST_API, MOVIE_API } from '../movie.api';
 @Injectable()
 export class MoviesService {
 
-  public API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTEyM2Y1YzFlNTA5Y2Q5NjU0YmQyYjEyN2UzMTQ1NSIsInN1YiI6IjU5YTIzYjRlOTI1MTQxNzdhNzAzNjRhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bfB9pSr_iGJa1PNpLC55Q6SYAohK5OGujZXGQAtJn0Q";
+  public API_TOKEN = "eyJ0eXAiOiJKV1AzNjRhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bfB9pSr_iGJa1PNpLC55Q6SYAohK5OGujZXGQAtJn0Q";
   public API_KEY = "8e123f5c1e509cd9654bd2b127e31455";
   
   constructor(private http: HttpClient) {}
@@ -19,8 +19,7 @@ export class MoviesService {
    getMovies(listID:number = 1, page:number = 1): Observable<any> {
 
     return this.http.get(`${MOVIELIST_API}/${listID}?page=${page}&api_key=${this.API_KEY}&language=pt_BR`, { headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'authorization': this.API_TOKEN,
+      'Content-Type': 'application/json;charset=utf-8',
     })}).pipe(
             tap(response => JSON.stringify(response))
           )
@@ -29,7 +28,6 @@ export class MoviesService {
   getMoiveByID(id:number): Observable<any> {
     return this.http.get(`${MOVIE_API}/${id}?api_key=${this.API_KEY}&language=pt_BR`, { headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'authorization': this.API_TOKEN,
     })}).pipe(
             tap(response => JSON.stringify(response))
           )
